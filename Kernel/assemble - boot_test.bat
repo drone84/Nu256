f3a3189d47c0777a1a7cf4d7c0798708f694bde9
Thomas Wilson <wilsontp@gmail.com>
2018-09-18 22:08:45 -0700

    ASSEMBLE THIS....

--- Kernel/assemble - boot_test.bat
+++ Kernel/assemble - boot_test.bat
@@ -1,5 +1,7 @@
 @echo off
 
+echo THIS ASSEMBLES CODE FOR THE PROTOTYPE. ASSEMBLES TO BANK 00
+
 del *.lst
 64tass boot_test.asm --long-address --intel-hex -o kernel.hex --list kernel.lst
 if errorlevel 1 goto end
